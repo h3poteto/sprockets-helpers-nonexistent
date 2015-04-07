cd86f49838645f7506a45f1a419c8dc3e507b3d3
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2015-04-07 12:06:12 +0900

    fix override method

--- lib/sprockets/helpers/nonexistent.rb
+++ lib/sprockets/helpers/nonexistent.rb
@@ -3,7 +3,7 @@ require "sprockets/helpers/nonexistent/version"
 require 'sprockets/helpers'
 
 module Sprockets::Helpers::RailsHelper
-  def stylesheet_link_tag(*sources)
+  def self.stylesheet_link_tag(*sources)
     sources.each do |stylesheet|
       unless stylesheet.kind_of?(Hash)
         raise Sprockets::FileNotFound, "#{stylesheet}.css" if (Rails.env.development? || Rails.env.test?) && asset_paths.asset_for(stylesheet, "css").blank?
@@ -12,7 +12,7 @@ module Sprockets::Helpers::RailsHelper
     super
   end
 
-  def javascript_include_tag(*sources)
+  def self.javascript_include_tag(*sources)
     sources.each do |javascript|
       unless javascript.kind_of?(Hash)
         raise Sprockets::FileNotFound, "#{javascript}.js" if (Rails.env.development? || Rails.env.test?) && asset_paths.asset_for(javascript, "js").blank?
