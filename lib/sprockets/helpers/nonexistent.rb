7c62f660e13af5ab70bb59e90b5e43b048fd746e
AkiraFukushima <akira.fukushima@crowdworks.co.jp>
2015-04-07 12:41:32 +0900

    fixes method name

--- lib/sprockets/helpers/nonexistent.rb
+++ lib/sprockets/helpers/nonexistent.rb
@@ -3,7 +3,7 @@ require "sprockets/helpers/nonexistent/version"
 require 'sprockets/helpers'
 
 module Sprockets::Helpers::RailsHelper
-  def self.stylesheet_link_tag(*sources)
+  def stylesheet_link_tag(*sources)
     sources.each do |stylesheet|
       unless stylesheet.kind_of?(Hash)
         raise Sprockets::FileNotFound, "#{stylesheet}.css" if (Rails.env.development? || Rails.env.test?) && asset_paths.asset_for(stylesheet, "css").blank?
@@ -12,7 +12,7 @@ module Sprockets::Helpers::RailsHelper
     super
   end
 
-  def self.javascript_include_tag(*sources)
+  def javascript_include_tag(*sources)
     sources.each do |javascript|
       unless javascript.kind_of?(Hash)
         raise Sprockets::FileNotFound, "#{javascript}.js" if (Rails.env.development? || Rails.env.test?) && asset_paths.asset_for(javascript, "js").blank?
